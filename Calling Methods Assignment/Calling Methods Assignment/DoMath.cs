fb1a4f5d1255b2557a1bd57173b759bbf657d1c9
Dar1ux <djankaus87@gmail.com>
2023-06-06 12:25:11 +0300

    Calling Methods Assignment

---
+++ Calling Methods Assignment/Calling Methods Assignment/DoMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class DoMath
+    {
+        public static int Square (int num1) // Defining a method with its Access Modifier, a Return Type, and Parameter
+        {
+            int square = num1 * num1;
+            return square;
+        }
+
+        public static int AddItself(int num1)
+        {
+            int addItself = num1 + num1;
+            return addItself;
+        }
+
+        public static int Negative(int num1)
+        {
+            int negative = num1 * -1;
+            return negative;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number:");
+            int number = Convert.ToInt32(Console.ReadLine()); // getting input from a user and converting to an integer value
+
+            // Calling different methods from DoMath class and printing out the result
+            Console.WriteLine(number + " squared equals " + DoMath.Square(number));
+            Console.WriteLine(number + " added to itself equals " + DoMath.AddItself(number));
+            Console.WriteLine(number + " negative value is " + DoMath.Negative(number));
+
+            Console.ReadLine(); // Waiting for user input before closing the window
+        }
+    }
+}
