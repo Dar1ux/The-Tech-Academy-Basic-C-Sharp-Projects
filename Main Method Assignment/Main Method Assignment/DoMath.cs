cf61f5debc540114614bdf0fbf780dbbe5512268
Dar1ux <djankaus87@gmail.com>
2023-06-07 10:36:33 +0300

    Main Method Assignment

---
+++ Main Method Assignment/Main Method Assignment/DoMath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class DoMath
+    {
+        // 1. Create a class. In that class, create a method that will take in an integer,
+        // create a math operation for this integer (addition, subtraction, etc.),
+        // then return the answer as an integer.
+        public int Operation (int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        // 3. Add a second method to the class with the same name that will take in a decimal,
+        // create a different math operation for it, then return the answer as an integer.
+        public int Operation(decimal number)
+        {
+            decimal result = number - 10;
+            return Convert.ToInt32(result);
+        }
+
+        // 5. Add a third method to the class, with the same name, that will take in a string,
+        // convert it to an integer if possible, do a different math operation to it, then
+        // return the answer as an integer.
+        public int Operation(string number)
+        {
+            try
+            {
+                int n = Convert.ToInt32(number);
+                int result = n / 2;
+                return result;
+            }
+            catch
+            {
+                Console.WriteLine("\nUnable to convert '{0}' to an integer", number);
+                return 0;
+            }            
+        }
+
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. In the Main() method of the console app, instantiate the class and call
+            // the one method, passing in an integer. Display the result to the screen.
+            DoMath math = new DoMath();
+            Console.WriteLine("13 squared equals " + math.Operation(13));
+
+            // 4. In the Main() method of the console app, instantiate the class and call
+            // the second method, passing in a decimal. Display the result to the screen.
+            Console.WriteLine("\n13.345 minus 10 without decimals equals " + math.Operation(13.345m));
+
+            // 6. In the Main() method of the console app, instantiate the class and call
+            // the third method, passing in a string that equates to an integer.
+            // Display the result to the screen.
+            Console.WriteLine("\nHalf of 24 is " + math.Operation("24"));
+
+            Console.ReadLine();
+        }
+    }
+}
