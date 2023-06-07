bdfe51e9520bf70d89b7524e99b83845d1a408f6
Dar1ux <djankaus87@gmail.com>
2023-06-07 12:25:09 +0300

    Class Method Assignment

---
+++ Class Method Assignment/Class Method Assignment/DoMath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    // 1. Create a class. In that class, create a void method that outputs an integer.
+    // Have the method divide the data passed to it by 2.
+    public class DoMath
+    {
+        public void Half(int number, out int result)
+        {
+            result = number / 2;
+        }
+
+        // 4. Create a method with output parameters.
+        public static void Version(out int versionMain, out int versionIteration)
+        {
+            versionMain = 1;
+            versionIteration = 14;
+        }
+
+        // 5. Overload a method.
+        public static void Version(out string versionMain, out string versionIteration)
+        {
+            versionMain = "1";
+            versionIteration = "14";
+        }
+
+        // 6. Declare a class to be static.
+        // See above
+    }
+}
---
+++ Class Method Assignment/Class Method Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. In the Main() method, instantiate that class.
+            DoMath math = new DoMath();
+
+            // 3. Have the user enter a number. Call the method on that number.
+            // Display the output to the screen. It should be the entered number, divided by two. 
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            math.Half(number, out int result);
+            Console.WriteLine("Half of your number is " + result);
+
+            Console.ReadLine(); // Waiting for user input before closing the window
+        }
+    }
+}
