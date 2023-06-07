26fed9b85ba9e2d10a58026df38b92b57024ea1f
Dar1ux <djankaus87@gmail.com>
2023-06-07 11:39:45 +0300

    Method Class Assignment Submission

---
+++ Method Class Assignment Submission/Method Class Assignment Submission/DoMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    // 1. Create a class. In that class, create a void method that takes two integers as parameters.
+    // Have the method do a math operation on the first integer and display the second integer to the screen. 
+    class DoMath
+    {
+        public void Operation(int number, int userID)
+        {
+            int total = (number * 3) + 17;
+            Console.WriteLine(userID);
+        }
+    }
+}
---
+++ Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. In the Main() method of the console app, instantiate the class.
+            DoMath math = new DoMath();
+
+            // 3. Call the method in the class, passing in two numbers. 
+            math.Operation(13, 100456);
+
+            // 4. Call the method in the class, specifying the parameters by name.
+            math.Operation(number: 24, userID: 100332);
+
+            Console.ReadLine(); // Waiting for user input before closing the window
+        }
+    }
+}
