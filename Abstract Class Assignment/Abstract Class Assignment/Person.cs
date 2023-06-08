011473bae122520fe16d94704bb70fb679db65ab
Dar1ux <djankaus87@gmail.com>
2023-06-08 11:56:43 +0300

    Abstract Class Assignment

---
+++ Abstract Class Assignment/Abstract Class Assignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    // 1. Create an abstract class called Person with two properties:
+    // string firstName and string lastName.
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // 2. Give it the method SayName().
+        public abstract void SayName();
+
+
+    }
+}
