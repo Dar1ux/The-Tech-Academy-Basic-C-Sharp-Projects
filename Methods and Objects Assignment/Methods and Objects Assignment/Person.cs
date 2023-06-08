8897c820479689a2c673dad8c9a1e2e426bae49f
Dar1ux <djankaus87@gmail.com>
2023-06-08 10:51:40 +0300

    Methods and Objects Assignment

---
+++ Methods and Objects Assignment/Methods and Objects Assignment/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    // 1. Create a class called Person and give it two properties, each of data type string.
+    // One called FirstName, the other LastName.
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        // 2. Give this class a void method called SayName() that takes no parameters and
+        // simply writes the person's full name to the console in the format of: “Name: [full name]”.
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
